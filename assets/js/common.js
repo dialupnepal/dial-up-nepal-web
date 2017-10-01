c25727b68d6d7b5cc5f009541dffe6832bd9bded
Dial up Nepal <32428305+dialupnepal@users.noreply.github.com>
2017-10-01 13:24:04 +0530

    Website

---
+++ assets/js/common.js
@@ -0,0 +1,10 @@
+//Fixed services menu 
+// $(function() {
+//     $(window).scroll(function(e) {
+//         if ($(this).scrollTop() > 380) {
+//             $('.services-list').addClass("services-list-fixed col-sm-3")
+//         } else {
+//             $('.services-list').removeClass("services-list-fixed col-sm-3")
+//         }
+//     });
+// });
